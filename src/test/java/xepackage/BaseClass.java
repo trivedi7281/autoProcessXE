dc85e86597c7cee5bb9624ab6ddba6e17d56b2ab
Akash Trivedi <trivedi7281@outlook.com>
2020-12-18 15:51:54 +0530

    NA

--- src/test/java/xepackage/BaseClass.java
+++ src/test/java/xepackage/BaseClass.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
